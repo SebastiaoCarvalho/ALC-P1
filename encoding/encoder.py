1bed91e35a2bd6a0359a14d2ff4acb7819360711
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-09-25 16:35:51 +0100

    add typing and remove pycache

--- encoding/encoder.py
+++ encoding/encoder.py
@@ -1,8 +1,9 @@
-import pysat
+from pysat.examples.rc2 import RC2
+from domain.flight import Flight
 
 class Encoder :
 
     @staticmethod
-    def encode(self, solver, flight_list) -> None :
+    def encode(self, solver : RC2, flight_list : list[Flight]) -> None :
         pass
 
