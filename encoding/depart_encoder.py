43cd78eb86109374b5ce9c78a57c02881ad0ae69
jiyiru <inesji@tecnico.ulisboa.pt>
2024-09-25 18:58:30 +0100

    add encoding: arrival and departure flight only once for each city

---
+++ encoding/depart_encoder.py
@@ -0,0 +1,16 @@
+from pysat.examples.rc2 import RC2
+from domain.flight import Flight
+from domain.city import City
+from encoding.encoder import Encoder
+from pysat.card import CardEnc, EncType
+
+class DepartEncoder(Encoder) :
+
+    def encode(self, solver : RC2, flight_list : list[Flight], city_dict: tuple[dict[str, City]], var_count: int) -> int :
+        for city in city_dict.keys():
+            lits = [flight.get_id() for flight in flight_list if flight.get_departure_city() == city]
+            enc = CardEnc.equals(lits, bound=1, top_id=var_count, encoding=EncType.seqcounter)
+            for clause in enc.clauses:
+                solver.add_clause(clause)
+            var_count += len(lits) - 1
+        return var_count
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from parsers.input_parser import parse_input
+from pysat.examples.rc2 import RC2
+from pysat.formula import WCNF
+from encoding.depart_encoder import DepartEncoder
+from encoding.arrival_encoder import ArrivalEncoder  
+
 
 city_map, flight_list = parse_input()
 print(city_map)
 print(flight_list)
+
+solver = RC2(WCNF())
+
+encoder = DepartEncoder()
+var_counter = encoder.encode(solver, flight_list, city_map, len(flight_list))
+
+encoder = ArrivalEncoder()
+var_counter = encoder.encode(solver, flight_list, city_map, var_counter)
