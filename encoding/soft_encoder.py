173ba6f5b8fd95d5229ce7997f60bd899dc1a497
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-09-30 21:40:35 +0100

    fix in soft encoding

--- encoding/soft_encoder.py
+++ encoding/soft_encoder.py
@@ -6,7 +6,6 @@ from encoding.encoder import Encoder
 class SoftEncoder(Encoder):
 
     def encode(self, solver : RC2, flight_list : list[Flight], city_dict: dict[str, City], var_count: int) -> int :
-        max_cost = (max(flight.get_cost() for flight in flight_list))
         for flight in flight_list:
-            #print([flight.get_id()], max_cost - flight.get_cost() + 1)
-            solver.add_clause([flight.get_id()], weight= (max_cost - flight.get_cost() +1))
+            #print([-flight.get_id()], weight=flight.get_cost())
+            solver.add_clause([-flight.get_id()], weight=flight.get_cost())
