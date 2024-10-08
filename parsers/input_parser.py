8d539cd4b953d182bc93032c8e95276bbbd9957a
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-10-08 16:32:57 +0100

    remove overpriced flights

--- parsers/input_parser.py
+++ parsers/input_parser.py
@@ -14,8 +14,10 @@ def parse_input() -> Tuple[Dict[str, City], List[Flight]]:
         city = parse_city()
         city_map[city.code] = city
     m : int = int(readline())
-    for i in range(m):
-        flight_list.append(parse_flight(i+1))
+    for _ in range(m):
+        flight_list = parse_flight(flight_list)
+    for i in range(len(flight_list)):
+        flight_list[i].id = i + 1
     return city_map, flight_list
 
 
@@ -33,8 +35,17 @@ def parse_city() -> City:
 def readline() -> str:
     return sys.stdin.readline().strip()
 
-def parse_flight(id) -> Flight:
+def parse_flight(flight_list : list[Flight]) -> Flight:
     flight_info = readline()
     day, departure_city, arrival_city, departure_time, arrival_time, cost = flight_info.split()
     cost = int(cost)
-    return Flight(id, day, departure_city, arrival_city, departure_time, arrival_time, cost)
+    flight = Flight(0, day, departure_city, arrival_city, departure_time, arrival_time, cost)
+    duplicates : list[Flight] = list(filter(lambda f : f.departure_city == flight.departure_city and f.arrival_city == flight.arrival_city and f.day == flight.day, flight_list))
+    if len(duplicates) > 0:
+        duplicates.append(flight)   
+        #remove all duplicates from flight_list
+        flight_list = list(filter(lambda f : f.departure_city != flight.departure_city or f.arrival_city != flight.arrival_city or f.day != flight.day, flight_list))
+        min_cost = min(list(map(lambda f: f.cost, duplicates)))
+        flight = Flight(0, day, departure_city, arrival_city, departure_time, arrival_time, min_cost)
+    flight_list.append(flight)
+    return flight_list
